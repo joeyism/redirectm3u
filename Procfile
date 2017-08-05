4393330e29179493b937f6bb59b676797ca02759
joeyism <sham.joey@gmail.com>
2017-08-05 11:29:58 -0400

    Updated for heroku and main page

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: redirectm3u
--- main.go
+++ main.go
@@ -1,71 +1,85 @@
 package main
 
-import(
-	"os"
-	"github.com/ushis/m3u"
-	"fmt"
-	"strings"
-	"net/http"
+import (
 	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+    "html/template"
+
+	"github.com/ushis/m3u"
 )
 
 const port = "8080"
 
-var shows = [...]string{ "comedy", "drama", "brain", "etc", "scifi" }
+var shows = [...]string{"comedy", "drama", "brain", "etc", "scifi"}
+var line = []byte{}
 
-func bracketHost(originalPath string) string{
+func mainHandler(w http.ResponseWriter, r *http.Request){
+	t, _ := template.ParseFiles("main.html")
+	t.Execute(w, nil)
+}
+
+func bracketHost(originalPath string) string {
 	newPath := "["
 	colon := strings.LastIndex(originalPath, ":")
-	newPath += originalPath[7:colon]+ "]" + originalPath[colon:]
+	newPath += originalPath[7:colon] + "]" + originalPath[colon:]
 	return newPath
 }
 
-func getTrack(resp *http.Response, globalRes http.ResponseWriter){
+func getTrack(resp *http.Response, globalRes http.ResponseWriter, req *http.Request) {
 	reader := bufio.NewReader(resp.Body)
-	for {
-		line, _ := reader.ReadBytes('\n')
-		sendMessage(resp, line, globalRes)
-	}
+    for {
+        line, _ = reader.ReadBytes('\n')
+        //fmt.Println(line)
+        sendMessage(resp, line, globalRes)
+    }
 }
 
-func sendMessage(resp *http.Response, c []byte, res http.ResponseWriter){
-	if f, ok:=res.(http.Flusher); ok {
-		w := c
-		for k, v := range resp.Header {
-			res.Header().Set(k, v[0])
-		}
-		res.Write(w)
-		f.Flush()
-	} else {
-		fmt.Println("no flush")
-	}
+func sendMessage(resp *http.Response, c []byte, res http.ResponseWriter) {
+    if f, ok := res.(http.Flusher); ok {
+        w := c
+        for k, v := range resp.Header {
+            res.Header().Set(k, v[0])
+        }
+        res.Write(w)
+        f.Flush()
+    } else {
+        fmt.Println("no flush")
+    }
 }
 
-func main(){
+func main() {
 
-	for _, show := range shows {
-		f, err := os.Open(show + ".m3u")
+    for _, show := range shows {
+        f, err := os.Open(show + ".m3u")
 
-		if err != nil { 
-			panic(err)
-		}
-		defer f.Close()
+        if err != nil {
+            panic(err)
+        }
+        defer f.Close()
 
-		pl, err := m3u.Parse(f)
-		if err != nil {
-			panic(err)
-		}
+        pl, err := m3u.Parse(f)
+        if err != nil {
+            panic(err)
+        }
 
-		for _, track := range pl {
-			//track.Path
-			http.HandleFunc("/" + show, func(res http.ResponseWriter, req *http.Request){
-				fmt.Println("hitting " + req.URL.Path)
-				resp, _:= http.Get(track.Path)
-				getTrack(resp, res)
-			})
+        for _, track := range pl {
 
-		}
-	}
-	fmt.Println("Listening to "+port)
-	fmt.Println(http.ListenAndServe(":"+port, nil))
+            //track.Path
+            http.HandleFunc("/"+show, func(res http.ResponseWriter, req *http.Request) {
+                fmt.Println("hitting " + req.URL.Path)
+                resp, _ := http.Get(track.Path)
+                getTrack(resp, res, req)
+            })
+
+        }
+    }
+
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    http.HandleFunc("/", mainHandler)
+    fmt.Println("Listening to " + port)
+    fmt.Println(http.ListenAndServe(":"+port, nil))
 }
